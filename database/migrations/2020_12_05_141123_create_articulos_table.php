7b01b51ee3b162b6bee9d24d4b06e0ff5edd8253
ensoncarranza <91918775+ensoncarranza@users.noreply.github.com>
2022-03-26 18:24:50 -0600

    Add files via upload

---
+++ database/migrations/2020_12_05_141123_create_articulos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('Derpartamento',10);
+            $table->string('descripcion',50);
+            $table->integer('Habitantes');
+            $table->string('Patrimonio cultural inmaterial',50);
+            $table->string('Patrimonio histórico',50);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articulos');
+    }
+}
